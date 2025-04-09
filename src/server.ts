c4014628e5033c6f6cb50b8bef68d86145f5fd43
Marcos Almeida Oliveira <marcosalmeidaoli439@gmail.com>
2025-04-08 21:44:07 -0300

    Adicionar rotas à aplicação

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,14 @@ import fastify from "fastify";
 
 const app = fastify()
 
+app.get('/', () => {
+    return 'Hello World'
+})
+
+app.get('/teste', () => {
+    return 'Hello teste'
+})
+
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running!')
 })
