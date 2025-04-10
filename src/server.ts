77a236a78a73469e9e0a0e9130625dd3a4a66a4f
Marcos Almeida Oliveira <marcosalmeidaoli439@gmail.com>
2025-04-09 22:14:22 -0300

    Add comentários backend

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,15 @@ import fastify from "fastify";
 
 const app = fastify()
 
+// Métodos HTTP: GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS, ...
+
+// Corpo da requisição (Request Body) 
+// Parâmetros de busca (Search Params / Query Params) 'http://localhost:3333/users?name=Diego' 
+// Parâmetros de rota (Route Params) -> Identificação de recursos 'DELETE http://localhost:3333/users/5 
+// Cabeçalhos (Headers) -> Contexto 
+
+// Semanticas Significado
+
 app.get('/', () => {
     return 'Hello World'
 })
